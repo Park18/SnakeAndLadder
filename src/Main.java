452c0e833087f6ded665c5744b0923b12b661d6f
Park18 <sowoomi35@naver.com>
2019-12-15 03:03:18 +0900

    프로젝트 복사
    기존의 레포지토리가 꼬여서 다시 생성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import game.GameCenter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        GameCenter gameCenter = new GameCenter();
+        gameCenter.play();
     }
 }
